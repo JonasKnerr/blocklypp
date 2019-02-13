6902403ff742eae477a1717eb3050b3ad623571b
jknerr <jknerr@uni-osnabrueck.de>
2019-02-13 11:02:35 +0100

    klassen funktionen hinzugefügt

--- blockly/blocks/oop.js
+++ blockly/blocks/oop.js
@@ -15,7 +15,7 @@ Blockly.Blocks["control_class"] = {
       .setCheck(null)
       .appendField("Attribute");
     this.appendStatementInput("Methode")
-      .setCheck("Methode")
+      .setCheck(["class_function_noreturn", "class_function_return"])
       .appendField("Methoden");
     //this.setInputsInline(false);
     this.setColour(230);
@@ -24,4 +24,94 @@ Blockly.Blocks["control_class"] = {
     this.setTooltip("");
     this.setHelpUrl("");
   }
+};
+Blockly.Blocks["class_function_return"] = {
+  init: function() {
+    var nameField = new Blockly.FieldTextInput("", Blockly.Procedures.rename);
+    nameField.setSpellcheck(false);
+    this.appendDummyInput()
+      .appendField(Blockly.Msg["PROCEDURES_DEFRETURN_TITLE"])
+      .appendField(nameField, "NAME")
+      .appendField("", "PARAMS");
+    this.appendValueInput("RETURN")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg["PROCEDURES_DEFRETURN_RETURN"]);
+    this.setMutator(new Blockly.Mutator(["procedures_mutatorarg"]));
+    if (
+      (this.workspace.options.comments ||
+        (this.workspace.options.parentWorkspace && this.workspace.options.parentWorkspace.options.comments)) &&
+      Blockly.Msg["PROCEDURES_DEFRETURN_COMMENT"]
+    ) {
+      this.setCommentText(Blockly.Msg["PROCEDURES_DEFRETURN_COMMENT"]);
+    }
+    this.setNextStatement(true, ["class_function_noreturn", "class_function_return"]);
+    this.setPreviousStatement(true, ["class_function_noreturn", "class_function_return"]);
+    this.setColour(Blockly.Msg["PROCEDURES_HUE"]);
+    this.setTooltip(Blockly.Msg["PROCEDURES_DEFRETURN_TOOLTIP"]);
+    this.setHelpUrl(Blockly.Msg["PROCEDURES_DEFRETURN_HELPURL"]);
+    this.arguments_ = [];
+    this.argumentVarModels_ = [];
+    this.setStatements_(true);
+    this.statementConnection_ = null;
+  },
+  setStatements_: Blockly.Blocks["procedures_defnoreturn"].setStatements_,
+  updateParams_: Blockly.Blocks["procedures_defnoreturn"].updateParams_,
+  mutationToDom: Blockly.Blocks["procedures_defnoreturn"].mutationToDom,
+  domToMutation: Blockly.Blocks["procedures_defnoreturn"].domToMutation,
+  decompose: Blockly.Blocks["procedures_defnoreturn"].decompose,
+  compose: Blockly.Blocks["procedures_defnoreturn"].compose,
+  getVars: Blockly.Blocks["procedures_defnoreturn"].getVars,
+  getProcedureDef: function() {
+    return [this.getFieldValue("NAME"), this.arguments_, true];
+  },
+  getVarModels: Blockly.Blocks["procedures_defnoreturn"].getVarModels,
+  renameVarById: Blockly.Blocks["procedures_defnoreturn"].renameVarById,
+  updateVarName: Blockly.Blocks["procedures_defnoreturn"].updateVarName,
+  displayRenamedVar_: Blockly.Blocks["procedures_defnoreturn"].displayRenamedVar_,
+  customContextMenu: Blockly.Blocks["procedures_defnoreturn"].customContextMenu,
+  callType_: "procedures_callreturn"
+};
+
+Blockly.Blocks["class_function_noreturn"] = {
+  init: function() {
+    var nameField = new Blockly.FieldTextInput("", Blockly.Procedures.rename);
+    nameField.setSpellcheck(false);
+    this.appendDummyInput()
+      .appendField(Blockly.Msg["PROCEDURES_DEFNORETURN_TITLE"])
+      .appendField(nameField, "NAME")
+      .appendField("", "PARAMS");
+    this.setMutator(new Blockly.Mutator(["procedures_mutatorarg"]));
+    if (
+      (this.workspace.options.comments ||
+        (this.workspace.options.parentWorkspace && this.workspace.options.parentWorkspace.options.comments)) &&
+      Blockly.Msg["PROCEDURES_DEFNORETURN_COMMENT"]
+    ) {
+      this.setCommentText(Blockly.Msg["PROCEDURES_DEFNORETURN_COMMENT"]);
+    }
+    this.setNextStatement(true, ["class_function_noreturn", "class_function_return"]);
+    this.setPreviousStatement(true, ["class_function_noreturn", "class_function_return"]);
+    this.setColour(Blockly.Msg["PROCEDURES_HUE"]);
+    this.setTooltip(Blockly.Msg["PROCEDURES_DEFNORETURN_TOOLTIP"]);
+    this.setHelpUrl(Blockly.Msg["PROCEDURES_DEFNORETURN_HELPURL"]);
+    this.arguments_ = [];
+    this.argumentVarModels_ = [];
+    this.setStatements_(true);
+    this.statementConnection_ = null;
+  },
+  setStatements_: Blockly.Blocks["procedures_defnoreturn"].setStatements_,
+  updateParams_: Blockly.Blocks["procedures_defnoreturn"].updateParams_,
+  mutationToDom: Blockly.Blocks["procedures_defnoreturn"].mutationToDom,
+  domToMutation: Blockly.Blocks["procedures_defnoreturn"].domToMutation,
+  decompose: Blockly.Blocks["procedures_defnoreturn"].decompose,
+  compose: Blockly.Blocks["procedures_defnoreturn"].compose,
+  getVars: Blockly.Blocks["procedures_defnoreturn"].getVars,
+  getProcedureDef: function() {
+    return [this.getFieldValue("NAME"), this.arguments_, false];
+  },
+  getVarModels: Blockly.Blocks["procedures_defnoreturn"].getVarModels,
+  renameVarById: Blockly.Blocks["procedures_defnoreturn"].renameVarById,
+  updateVarName: Blockly.Blocks["procedures_defnoreturn"].updateVarName,
+  displayRenamedVar_: Blockly.Blocks["procedures_defnoreturn"].displayRenamedVar_,
+  customContextMenu: Blockly.Blocks["procedures_defnoreturn"].customContextMenu,
+  callType_: "procedures_callreturn"
 };
